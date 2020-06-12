6cba81a225e4d8c35d9c9794884f317852b4c15f
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-06-12 13:14:58 -0400

    ring buffer passing

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,9 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node:
+            while node.get_next:
+                return self.reverse_list(node, prev)
+            self.add_to_head(node.value)
+            while node.prev:
+                node.set_next(node.prev)
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.count = -1
 
     def append(self, item):
-        pass
+       
+        if len(self.storage) == self.capacity:
+            if (self.count + 1) == self.capacity:
+                self.count = 0
+            else:
+                self.count += 1
+            self.storage.pop(self.count)
+            self.storage.insert(self.count, item)
+                
+        else:
+            self.storage.append(item)
 
     def get(self):
-        pass
+        return self.storage
